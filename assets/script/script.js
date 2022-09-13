183f51643384a9d31330814f54fa79d4c6e82d29
Aurélien <aurelien.strimelle@bstorm.be>
2022-09-13 14:41:26 +0200

    Finish

--- assets/script/script.js
+++ assets/script/script.js
@@ -63,8 +63,9 @@ btnValid.addEventListener('click', () => {
     btnSave.innerText = 'Enregister';
     form2.appendChild(btnSave);
 
-    // Sauvegarde des score
+
     btnSave.addEventListener('click', () => {
+        // Sauvegarde des score
         for (let k = 0; k < joueurs.length; k++) {
 
             const inputScore = document.getElementById(`score_${joueurs[k]}`);
@@ -77,9 +78,61 @@ btnValid.addEventListener('click', () => {
             scoreJoueur[k] += score;  // scoreJoueur[k] = scoreJoueur[k] + score
         }
         console.table(scoreJoueur);
-        count++;
 
+        count++;
         NomMatch.innerText = `Match n° ${count}`;
+
+        // Passer à l'ecran de resultat
+        if (count > 3) {
+            // Formulaire des scores masqué !
+            form2.classList.add('hidden');
+
+            // Trouvé la valeur la plus faible → « Winner » 
+            // - Initialisation du meilleur score (premier joureur)
+            let lowerScrore = scoreJoueur[0];
+
+            // - Comparaison avec les autres scores
+            for (let y = 1; y < scoreJoueur.length; y++) {
+                if(lowerScrore > scoreJoueur[y]) {
+                    lowerScrore = scoreJoueur[y];
+                }
+            }
+
+            // Trouvé les indexes des joueurs qui ont le score le plus faible
+            let winners = [];
+
+            for(let x = 0; x < scoreJoueur.length; x++) {
+
+                if(lowerScrore === scoreJoueur[x]) {
+                    winners.push(x);
+                }
+            }
+            console.log('Winners', winners);
+
+
+            // Affichage du resultat
+            const titleResult = document.createElement('h3');
+            zoneResult.appendChild(titleResult);
+
+            if(winners.length === 1) {
+                const index = winners[0];
+                const winner = joueurs[index];
+
+                titleResult.innerText = `Le gagnant est ${winner} avec un score de ${lowerScrore}`
+            }
+            else {
+                titleResult.innerText = `Meilleur score ${lowerScrore}. Liste des gagnants : `;
+
+                for(const indiceWinner of winners) {
+
+                    const winner = joueurs[indiceWinner];
+                
+                    const baliseResult = document.createElement('p');
+                    baliseResult.innerText = winner;
+                    zoneResult.appendChild(baliseResult); 
+                }
+            }
+        }
     });
 })
 //
